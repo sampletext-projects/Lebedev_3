978514845791f523919bdfb9427d093d2930dffb
sampletext32 <sampletext32@bk.ru>
2020-04-21 19:27:21 +0300

    Add project

---
+++ Lebedev_3/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	cout << "Выполнил Лебедев П.В. (УМЛ-112)" << endl;
+	cout << "Программа для расчёта количества амёб" << endl;
+
+	int count = 1;
+	int hours = 0;
+	while (hours <= 48)
+	{
+		cout << "День " << hours / 24 << ", час " << hours % 24 << ", амёб: " << count << "\n";
+		hours += 3;
+		count *= 2;
+	}
+
+	system("pause");
+	return 0;
+}
